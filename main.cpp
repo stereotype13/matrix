4b2a1d477910266b666f5e04ad2b30d7a25df986
Joe Smith <stereotype13@hotmail.com>
2016-10-26 13:24:20 -0700

    Second commit

--- main.cpp
+++ main.cpp
@@ -6,16 +6,20 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	Matrix<float> matrix1(3, 2);
-	matrix1 = {{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15}};
+	Matrix<float> matrix1;
+	matrix1 = {
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11,12,13,14,15}
+	};
 	matrix1.print();
 
-	Matrix<float> matrix2(2, 2);
+	Matrix<float> matrix2;
 	matrix2 = {
-		{1,2},
-		{3,4},
-		{5,6},
-		{7,8},
+		{1, 2},
+		{3, 4},
+		{5, 6},
+		{7, 8},
 		{9, 10}
 	};
 
@@ -35,5 +39,13 @@ int main(int argc, char const *argv[])
 
 	matrix3 = matrix3 * 2.6;
 	matrix3.print();
+
+	matrix3 = matrix3 * matrix3;
+	matrix3.print();
+
+	Matrix<int> matrix5({{10, 11},{12, 13}});
+	matrix5.print();
+
+	matrix3.dot(matrix5).print();
 	return 0;
 }
--- matrix.h
+++ matrix.h
@@ -10,9 +10,31 @@ private:
 	int rows, columns;
 	std::vector<std::vector<T>> matrix;
 
+	void setSize(int rows, int columns) {
+		size.rows = rows;
+		size.columns = columns;
+	}
+
 public:
+	struct Size {
+		int rows, columns;
+		int operator[](int index) {
+			switch (index) {
+				case 0:
+				return rows;
+				break;
+
+				case 1:
+				return columns;
+			}
+		}
+	};
+
+	Size size;
+
 	Matrix() : rows{1}, columns{1} {
 		this->matrix = {{0}};
+		setSize(rows, columns);
 	}
 
 	Matrix(int rows, int columns) : rows{rows}, columns{columns} {
@@ -25,14 +47,33 @@ public:
 			}
 			this->matrix.push_back(row);
 		}
+		setSize(rows, columns);
 	}
 
-	void operator=(const Matrix<T>& right) {
+	Matrix(const Matrix<T>& right) : rows{right.rows}, columns{right.columns}, matrix{right.matrix} {}
+
+	Matrix(Matrix<T>&& right) {
+		std::swap(this->rows, right.rows);
+		std::swap(this->columns, right.columns);
+		std::swap(this->matrix, right.matrix);
+		setSize(rows, columns);
+	}
+
+	Matrix(std::vector<std::vector<T>>&& right) {
+		rows = right.size();
+		columns = right[0].size();
+
+		this->matrix = right;
+		setSize(rows, columns);
+	}
+
+	void operator=(Matrix<T>& right) {
 		rows = right.rows;
 		columns = right.columns;
+		setSize(rows, columns);
 
 		this->matrix.resize(rows);
-		for (const auto& row : this->matrix) {
+		for (auto& row : this->matrix) {
 			row.resize(columns);
 		}
 
@@ -43,28 +84,25 @@ public:
 				this->matrix[i][j] = right[i][j];
 			}
 		}
+	}
+
+	void operator=(Matrix<T>&& right) {
+		std::swap(rows, right.rows);
+		std::swap(columns, right.columns);
+		std::swap(matrix, right.matrix);
+		setSize(rows, columns);
 	}
 
 	void operator=(std::vector<std::vector<T>>&& right) {
 		rows = right.size();
 		columns = right[0].size();
 
-		this->matrix.resize(rows);
-		for (const auto& row : this->matrix) {
-			row.resize(columns);
-		}
-
-		for (int i = 0; i < rows; ++i)
-		{
-			for (int j = 0; j < columns; ++j)
-			{
-				this->matrix[i][j] = right[i][j];
-			}
-		}
+		std::swap(matrix, right);
+		setSize(rows, columns);
 
 	}
 
-	Matrix<T> dot(const Matrix<T>& right) {
+	Matrix<T> dot(Matrix<T>& right) {
 		Matrix<T> result(this->rows, right.columns);
 		
 		for (int i = 0; i < result.columns; ++i)
@@ -84,8 +122,77 @@ public:
 		return result;
 	} 
 
-	std::vector<T>& operator[](int index) {
+	template <typename T2>
+	Matrix<T> dot(Matrix<T2>& right) {
+		Matrix<T> result(this->rows, right.size[1]);
+		
+		for (int i = 0; i < result.columns; ++i)
+		{
+			for (int k = 0; k < this->rows; ++k)
+			{
+				T sum = 0;
+				for (int m = 0; m < this->columns; ++m)
+				{
+					sum += this->matrix[k][m] * (T)right[m][i];
+				}
+				result[k][i] = sum;
+				
+			}
+		}
+
+		return result;
+	} 
+
+	std::vector<T>& operator[](int index)  {
 		return this->matrix[index];
+	}
+
+	Matrix<T> operator+(Matrix<T>& right) {
+		Matrix<T> result;
+		result.matrix = this->matrix;
+		result.rows = this->rows;
+		result.columns = this->columns;
+
+		for (int i = 0; i < this->rows; ++i)
+		{
+			for (int j = 0; j < this->columns; ++j)
+			{
+				result[i][j] += right[i][j];
+			}
+		}
+		return result;
+	}
+
+	Matrix<T> operator-(Matrix<T>& right) {
+		Matrix<T> result;
+		result.matrix = this->matrix;
+		result.rows = this->rows;
+		result.columns = this->columns;
+
+		for (int i = 0; i < this->rows; ++i)
+		{
+			for (int j = 0; j < this->columns; ++j)
+			{
+				result[i][j] -= right[i][j];
+			}
+		}
+		return result;
+	}
+
+	Matrix<T> operator*(Matrix<T>& right) {
+		Matrix<T> result;
+		result.matrix = this->matrix;
+		result.rows = this->rows;
+		result.columns = this->columns;
+
+		for (int i = 0; i < this->rows; ++i)
+		{
+			for (int j = 0; j < this->columns; ++j)
+			{
+				result[i][j] *= right[i][j];
+			}
+		}
+		return result;
 	}
 
 	Matrix<T> operator*(double n) {
